e352e38af6d643855659745798a12ccbfcfcde1c
PauloVitor007 <paulo.hermogens@academico.ifpb.edu.br>
2024-11-25 16:08:04 +0000

    Resolvendo Atividade

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+const http = require('http');
+const moment = require('moment');
+const fs = require('fs')
 
+const server = http.createServer((req, res) => {
+
+    try {
+        const data = fs.readFileSync(__dirname + '/docxs.txt', 'utf8')
+        console.log(data)
+    }catch (err) {
+        console.error(err)
+    }
+    res.writeHead(200, { 'Content-Type': 'text/plain' }); 
+    res.end('Olá, Mundo'); 
+
+});
+
+server.listen(3000, () => {
+    console.log('Servidor rodando em: http://localhost:3000');
+    console.log(`Data Atual: ${moment().format("DD/MMM/YYYY HH:mm")}`);
+});
